44bbc078baa9d0e819de5d79ba2759d4cb63eb4b
Douglas-g-s-Rocha <grocha0612@gmail.com>
2023-04-12 22:52:38 -0300

    99% do cadastro feito, precisa apenas retirar o bug da senha

--- src/Operações/Cadastro_De_User.java
+++ src/Operações/Cadastro_De_User.java
@@ -1,7 +1,6 @@
 package Operações;
 
 
-
 import java.util.Scanner;
 
 public class Cadastro_De_User {
@@ -9,14 +8,20 @@ public class Cadastro_De_User {
 
     public String executarCadastro_De_User() {
 
-
+        int Ano = 2007;
         Scanner scanner = new Scanner(System.in);
 
 
+//CADASTRO
+
         System.out.println("Cadastro BANCO TIGAS");
+        System.out.println("OBS: Idade minima para cadastro é de 16 anos");
 
         System.out.println("Digite o seu nome completo: ");
         String Nome = scanner.nextLine();
+
+        System.out.println("Digite seu Email: ");
+        String email = scanner.nextLine();
 
         System.out.println("Digite o seu CPF: ");
         String CPF = scanner.nextLine();
@@ -28,33 +33,51 @@ public class Cadastro_De_User {
         String mesNasc = scanner.nextLine();
 
         System.out.println("Digite o ano do seu nascimento: ");
-        String anoNasc = scanner.nextLine();
+        int anoNasc = scanner.nextInt();
 
-        System.out.println("Digite seu Email: ");
-        String Email = scanner.nextLine();
 
+//LIMITAÇÃO DE IDADE
+        int idade = anoNasc - Ano;
+        if (anoNasc >= idade ) {
+
+        }else {
+            System.out.println("Idade indisponivel para cadastro! ");
+        }
+
+
+//SENHA
         System.out.println("Digite sua senha: ");
         String Password = scanner.nextLine();
 
+
+
+//CONFIRMAÇÃO DE SENHA
         System.out.println("Confirme sua senha: ");
         String confPass = scanner.nextLine();
+
         if (confPass == Password) {
 
-        } else {
+            System.out.println("Digite o seu endereço: ");
+            String endereco = scanner.nextLine();
 
-                System.out.println("Senha incorreta! Digite novamente!");
+            System.out.println("Digite seu numero para possiveis contatos: ");
+            int ctt = scanner.nextInt();
 
+        } else if (confPass != Password) {
+
+            System.out.println("Senha incorreta! Digite novamente!");
             System.out.println("Confirme sua senha: ");
             confPass = scanner.nextLine();
 
+        }else
+        {
 
+                System.out.println("Senha incorreta! Digite novamente!");
+                System.out.println("Confirme sua senha: ");
+                confPass = scanner.nextLine();
 
             }
-        System.out.println("Digite o seu endereço: ");
-        String endereco = scanner.nextLine();
 
-        System.out.println("Digite seu numero para possiveis contatos: ");
-        int ctt = scanner.nextInt();
 
       return null;
     }
